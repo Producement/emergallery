3a556f5ed004e575d7c7472be62527c4d20de542
Erko Risthein <erko@risthein.ee>
2018-12-13 12:14:13 +0200

    Added cors

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,15 +1,20 @@
 import * as functions from 'firebase-functions';
 import * as Twilio from 'twilio';
+import * as cors from 'cors';
+
+const corsHandler = cors({origin: true});
 
 export const messages = functions.https.onRequest((request, response) => {
- const accountSid = functions.config().twilio.account.sid;
- const authToken = functions.config().twilio.auth.token;
- const client = Twilio(accountSid, authToken);
+  corsHandler(request, response, () => {
+    const accountSid = functions.config().twilio.account.sid;
+    const authToken = functions.config().twilio.auth.token;
+    const client = Twilio(accountSid, authToken);
 
- console.log(request.query.message, request.query.to);
+    console.log(request.query.message, request.query.to);
 
- client.messages
-     .create({from: '+37259120139', body: request.query.message, to: request.query.to})
-     .then(message => response.send(message))
-     .catch(error => response.send(error));
+    client.messages
+        .create({from: '+37259120139', body: request.query.message, to: request.query.to})
+        .then(message => response.send(message))
+        .catch(error => response.send(error));
+  });
 });
