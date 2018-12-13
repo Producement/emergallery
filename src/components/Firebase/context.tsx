71a2e08501e99caa9475a12a513cb7aa0add2155
Maido Käära <m@maido.io>
2018-12-13 09:58:07 +0200

    Exporting firestore

--- src/components/Firebase/context.tsx
+++ src/components/Firebase/context.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import * as React from 'react';
 import Firebase from './firebase';
 
 const FirebaseContext = React.createContext<Firebase | null>(null);
+
+export const withFirebase = (Component: typeof React.Component) => (
+  props: any
+) => (
+  <FirebaseContext.Consumer>
+    {firebase => <Component {...props} firebase={firebase} />}
+  </FirebaseContext.Consumer>
+);
 
 export default FirebaseContext;
