94030b4b71388481d73584a05f3b6d8e266e12f9
Erko Risthein <erko@risthein.ee>
2018-12-13 12:37:22 +0200

    Add SMS button

--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -3,6 +3,16 @@ import uuid from 'uuid/v4';
 import { withFirebase } from '../Firebase';
 
 class Upload extends React.Component<any, any> {
+  render() {
+    return (
+      <div className="App">
+        <input type="file" name="" id="" onChange={this.handleselectedFile} />
+        <button onClick={this.handleUpload}>Upload</button>
+        <button onClick={this.sendSms}>Send SMS to Tõnu</button>
+      </div>
+    );
+  }
+
   handleselectedFile = event => {
     this.setState({
       selectedFile: event.target.files[0],
@@ -31,13 +41,12 @@ class Upload extends React.Component<any, any> {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <input type="file" name="" id="" onChange={this.handleselectedFile} />
-        <button onClick={this.handleUpload}>Upload</button>
-      </div>
-    );
+  private sendSms() {
+    fetch(
+      'http://localhost:5000/g48riik/us-central1/messages/messages?to=+37253044744&message=häirekeskus!!!11'
+    )
+      .then(response => console.log(response.body))
+      .catch(error => console.log(error));
   }
 }
 
