45fa93f6035539721b59ae912d8e1d12402c23c5
circleci <circleci@producement.com>
2018-12-13 21:11:42 +0000

    Updates

--- precache-manifest.2613a3c220206b95c30da3934c9286dd.js
+++ precache-manifest.2613a3c220206b95c30da3934c9286dd.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4c9a85bab90e0c69d582",
+    "revision": "50826c3b7a43150b346b",
     "url": "/static/css/main.5981f760.chunk.css"
   },
   {
-    "revision": "4c9a85bab90e0c69d582",
-    "url": "/static/js/main.4c9a85ba.chunk.js"
+    "revision": "50826c3b7a43150b346b",
+    "url": "/static/js/main.50826c3b.chunk.js"
   },
   {
     "revision": "bf777dcc41f9576cd6b4",
@@ -32,19 +32,19 @@ self.__precacheManifest = [
     "url": "/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
-  },
-  {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "b190d4f2bb9a6197bb4d78b855619588",
+    "revision": "e36434212b4c96a1f9fb085173107bf1",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08deecbadea4b62aad23e8ca085bf5da.js"
+  "/precache-manifest.2613a3c220206b95c30da3934c9286dd.js"
 );
 
 workbox.clientsClaim();
