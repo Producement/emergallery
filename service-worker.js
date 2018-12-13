6199727664af1a63a6f2e25445bc3b5436b90d98
circleci <circleci@producement.com>
2018-12-13 21:04:37 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.129b89c3fa3b8e65280ce885216dcc70.js"
+  "/precache-manifest.08deecbadea4b62aad23e8ca085bf5da.js"
 );
 
 workbox.clientsClaim();
