f7307277aa1f3059a1b0c4f74200f90e22388ff3
Erko Risthein <erko@risthein.ee>
2018-12-13 16:02:41 +0200

    Fix image aspect ratio

--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -7,19 +7,56 @@ class Upload extends React.Component<any, any> {
   render() {
     return (
       <div className="App">
-        <input type="file" name="" id="" onChange={this.handleselectedFile} />
+        <div className="input-group mb-3">
+          <div className="custom-file">
+            <input
+              type="file"
+              className="custom-file-input"
+              id="inputGroupFile02"
+              onChange={this.handleSelectedFile}
+            />
+            <label
+              className="custom-file-label"
+              htmlFor="inputGroupFile02"
+              aria-describedby="inputGroupFileAddon02"
+            >
+              Upload picture
+            </label>
+          </div>
+          <div className="input-group-append">
+            <span className="input-group-text" id="inputGroupFileAddon02">
+              Upload
+            </span>
+          </div>
+        </div>
         <InitiatorPeer />
       </div>
     );
   }
 
-  handleselectedFile = (event: any) => {
+  handleSelectedFile = (event: any) => {
     const selectedFile = event.target.files[0];
     const { firebase, match } = this.props;
     const eventId = match.params.id;
     const storage = firebase.storage();
     const id = uuid();
     const ref = storage.ref().child(id);
+
+    let width = 1;
+    let height = 1;
+    if (selectedFile) {
+      const img = new Image();
+
+      img.src = window.URL.createObjectURL(selectedFile);
+
+      img.onload = function() {
+        width = img.naturalWidth;
+        height = img.naturalHeight;
+
+        window.URL.revokeObjectURL(img.src);
+      };
+    }
+
     ref.put(selectedFile).then(function(snapshot) {
       console.log('Saved file');
       snapshot.ref.getDownloadURL().then(function(url) {
@@ -30,7 +67,7 @@ class Upload extends React.Component<any, any> {
           .doc(eventId)
           .collection('images')
           .doc(id)
-          .set({ width: 1, height: 1, src: url });
+          .set({ width: width, height: height, src: url });
       });
     });
   };
