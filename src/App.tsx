2f1390eb3bdba066d94bb44340f019db2858a274
Maido Käära <m@maido.io>
2018-12-13 11:23:58 +0200

    Fetch event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import Gallery from './components/Gallery';
+import { withFirebase } from './components/Firebase';
 import './App.css';
 
-class App extends Component {
+class App extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = { event: { id: 'S7FXwA766Nay1ymu0dcN' } };
+  }
+
+  componentDidMount() {
+    const hash = window.location.hash || '#S7FXwA766Nay1ymu0dcN';
+    this.props.firebase
+      .firestore()
+      .collection('events')
+      .doc(hash.substring(1))
+      .onSnapshot((snapshot: any) => {
+        this.setState({
+          event: snapshot.data()
+        });
+      });
+  }
+
   render() {
-    return <Gallery id="S7FXwA766Nay1ymu0dcN" />;
+    return <Gallery id={this.state.event.id} />;
   }
 }
 
-export default App;
+export default withFirebase(App);
