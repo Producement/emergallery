1d18fce4d94b5fe50fddfb2cb344125c03b92d07
Erko Risthein <erko@risthein.ee>
2018-12-13 11:46:58 +0200

    Added the messages endpoint to firebase

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as Twilio from 'twilio';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+export const messages = functions.https.onRequest((request, response) => {
+ const accountSid = functions.config().twilio.account.sid;
+ const authToken = functions.config().twilio.auth.token;
+ const client = Twilio(accountSid, authToken);
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+ console.log(request.query.message, request.query.to);
+
+ client.messages
+     .create({from: '+37259120139', body: request.query.message, to: request.query.to})
+     .then(message => response.send(message))
+     .catch(error => response.send(error));
+}
