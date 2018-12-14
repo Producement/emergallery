712bc8f2ce204796bd14b4903bd8f67386bd8fbb
Erko Risthein <erko@risthein.ee>
2018-12-14 12:16:13 +0200

    Added proper links to navigation

--- src/components/Gallery/gallery.tsx
+++ src/components/Gallery/gallery.tsx
@@ -7,6 +7,7 @@ import { ReceivingPeer } from '../Video';
 import './Gallery.css';
 import man from './man01.png';
 import bull from '../../bull.svg';
+import { HashLink } from 'react-router-hash-link';
 
 class SubGallery extends Component<any, any> {
   constructor(props) {
@@ -42,7 +43,7 @@ class SubGallery extends Component<any, any> {
 
   render() {
     return (
-      <section>
+      <section id="pildid">
         <div className="row section">
           <div className="col-lg-12 text-center">
             <div className="title-box">
@@ -221,86 +222,28 @@ class Gallery extends Component<any, any> {
               <div className="profile-usermenu">
                 <ul className="nav flex-column">
                   <li className="nav-item">
-                    <a className="nav-link active" href="#">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="feather feather-layers"
-                      >
-                        <polygon points="12 2 2 7 12 12 22 7 12 2" />
-                        <polyline points="2 17 12 22 22 17" />
-                        <polyline points="2 12 12 17 22 12" />
-                      </svg>
+                    <HashLink className="nav-link active" to="#pildid">
+                      <i className="fa fa-picture-o" aria-hidden="true" />
                       Pildid <span className="sr-only">(current)</span>
-                    </a>
+                    </HashLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="feather feather-home"
-                      >
-                        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                        <polyline points="9 22 9 12 15 12 15 22" />
-                      </svg>
-                      Video <span className="sr-only">(current)</span>
-                    </a>
+                    <HashLink className="nav-link" to="#video">
+                      <i className="fa fa-video-camera" aria-hidden="true" />
+                      Video
+                    </HashLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="feather feather-file"
-                      >
-                        <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z" />
-                        <polyline points="13 2 13 9 20 9" />
-                      </svg>
+                    <HashLink className="nav-link" to="#waze">
+                      <i className="fa fa-car" aria-hidden="true" />
                       Waze
-                    </a>
+                    </HashLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="feather feather-shopping-cart"
-                      >
-                        <circle cx="9" cy="21" r="1" />
-                        <circle cx="20" cy="21" r="1" />
-                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
-                      </svg>
+                    <HashLink className="nav-link" to="#twitter">
+                      <i className="fa fa-twitter" aria-hidden="true" />
                       Twitter
-                    </a>
+                    </HashLink>
                   </li>
                 </ul>
               </div>
@@ -313,7 +256,7 @@ class Gallery extends Component<any, any> {
                   <SubGallery photos={this.state.images} />
                 )}
 
-                <section>
+                <section id="video">
                   <div className="row section">
                     <div className="col-lg-12 text-center">
                       <div className="title-box">
@@ -333,7 +276,7 @@ class Gallery extends Component<any, any> {
 
                 {this.getLatitude() && this.getLongitude() && (
                   <div>
-                    <section>
+                    <section id="waze">
                       <div className="row section">
                         <div className="col-lg-12 text-center">
                           <div className="title-box">
@@ -348,7 +291,7 @@ class Gallery extends Component<any, any> {
                           </div>
                         </div>
                       </div>
-                      <div className="embed-responsive embed-responsive-21by9">
+                      <div className="embed-responsive embed-responsive-16by9">
                         <iframe
                           allowFullScreen
                           frameBorder="0"
@@ -357,7 +300,7 @@ class Gallery extends Component<any, any> {
                       </div>
                     </section>
 
-                    <section>
+                    <section id="twitter">
                       <div className="row section">
                         <div className="col-lg-12 text-center">
                           <div className="title-box">
@@ -372,7 +315,7 @@ class Gallery extends Component<any, any> {
                           </div>
                         </div>
                       </div>
-                      <div className="embed-responsive embed-responsive-21by9">
+                      <div className="embed-responsive embed-responsive-16by9">
                         <iframe
                           allowFullScreen
                           frameBorder="0"
