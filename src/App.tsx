c5657f0776effbc1fb77091f38c7b27e033eaf59
Maido Käära <m@maido.io>
2018-12-14 09:54:43 +0200

    Removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,6 @@ class App extends Component<any, any> {
           <Route exact path="/" component={Index} />
           <Route path="/event/:id" component={Gallery} />
           <Route path="/upload/:id" component={Upload} />
-          <footer className="section bg-gray footer">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="footer-alt">
-                    <div className="float-left pull-none ">
-                      <span className="navbar-brand logo">
-                        <img src={bull} alt="" /> <span>Producement</span>
-                      </span>
-                    </div>
-                    <div className="float-right pull-none ">
-                      <p className="pull-right text-muted m-b-0">
-                        2018 &copy;{' '}
-                        <a href="https://producement.com">Producement.com</a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </footer>
         </div>
       </HashRouter>
     );
