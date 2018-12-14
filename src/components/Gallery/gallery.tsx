ffdaae6dd3f6ed1f7089c49d9b9d52499fe05fb3
Erko Risthein <erko@risthein.ee>
2018-12-14 11:44:27 +0200

    Added sidebar to the gallery page

--- src/components/Gallery/gallery.tsx
+++ src/components/Gallery/gallery.tsx
@@ -191,111 +191,203 @@ class Gallery extends Component<any, any> {
     }
     return (
       <div className="container">
-        {this.state.images.length > 0 && (
-          <SubGallery photos={this.state.images} />
-        )}
+        <div className="row profile">
+          <div className="col-md-3 sidebar">
+            <div className="sidebar-sticky">
+              <div className="main-section text-center">
+                <div className="profile-header" />
+                <div className="row user-detail">
+                  <div className="col-lg-12 col-sm-12 col-12">
+                    <img src={man} className="rounded-circle img-thumbnail" />
+                    <h5>{this.getPhone()}</h5>
+                    <p>
+                      <i className="fa fa-map-marker" aria-hidden="true" />{' '}
+                      {this.getAddress() || 'Ei ole saadaval'}
+                    </p>
+                    <hr />
+                    <p>
+                      <i className="fa fa-compass" aria-hidden="true" />{' '}
+                      {this.getLatitude() || 'Ei ole saadaval'}
+                    </p>
+                    <p>
+                      <i className="fa fa-globe" aria-hidden="true" />{' '}
+                      {this.getLongitude() || 'Ei ole saadaval'}
+                    </p>
+                  </div>
+                </div>
+                <div className="user-social-detail" />
+              </div>
 
-        <section>
-          <div className="row section">
-            <div className="col-lg-12 text-center">
-              <div className="title-box">
-                <p className="title-alt">Video</p>
-                <h3 className="fadeIn animated wow" data-wow-delay=".1s">
-                  Live videoülekanne
-                </h3>
-                <div className="border" />
+              <div className="profile-usermenu">
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="feather feather-layers"
+                      >
+                        <polygon points="12 2 2 7 12 12 22 7 12 2" />
+                        <polyline points="2 17 12 22 22 17" />
+                        <polyline points="2 12 12 17 22 12" />
+                      </svg>
+                      Pildid <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="feather feather-home"
+                      >
+                        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                        <polyline points="9 22 9 12 15 12 15 22" />
+                      </svg>
+                      Video <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="feather feather-file"
+                      >
+                        <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z" />
+                        <polyline points="13 2 13 9 20 9" />
+                      </svg>
+                      Waze
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="feather feather-shopping-cart"
+                      >
+                        <circle cx="9" cy="21" r="1" />
+                        <circle cx="20" cy="21" r="1" />
+                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
+                      </svg>
+                      Twitter
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
-          <ReceivingPeer />
-        </section>
+          <div className="col-md-9">
+            <div className="profile-content">
+              <div className="container">
+                {this.state.images.length > 0 && (
+                  <SubGallery photos={this.state.images} />
+                )}
 
-        <div className="row section">
-          <div className="col-lg-12 text-center">
-            <div className="title-box">
-              <p className="title-alt">Inimene</p>
-              <h3 className="fadeIn animated wow" data-wow-delay=".1s">
-                Inimese profiil
-              </h3>
-              <div className="border" />
+                <section>
+                  <div className="row section">
+                    <div className="col-lg-12 text-center">
+                      <div className="title-box">
+                        <p className="title-alt">Video</p>
+                        <h3
+                          className="fadeIn animated wow"
+                          data-wow-delay=".1s"
+                        >
+                          Live videoülekanne
+                        </h3>
+                        <div className="border" />
+                      </div>
+                    </div>
+                  </div>
+                  <ReceivingPeer />
+                </section>
+
+                {this.getLatitude() && this.getLongitude() && (
+                  <div>
+                    <section>
+                      <div className="row section">
+                        <div className="col-lg-12 text-center">
+                          <div className="title-box">
+                            <p className="title-alt">Waze</p>
+                            <h3
+                              className="fadeIn animated wow"
+                              data-wow-delay=".1s"
+                            >
+                              Sündmused kaardil
+                            </h3>
+                            <div className="border" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="embed-responsive embed-responsive-21by9">
+                        <iframe
+                          allowFullScreen
+                          frameBorder="0"
+                          src="https://embed.waze.com/iframe?zoom=14&lat=59.438698&lon=24.729117&ct=livemap"
+                        />
+                      </div>
+                    </section>
+
+                    <section>
+                      <div className="row section">
+                        <div className="col-lg-12 text-center">
+                          <div className="title-box">
+                            <p className="title-alt">Twitter</p>
+                            <h3
+                              className="fadeIn animated wow"
+                              data-wow-delay=".1s"
+                            >
+                              Säutsud kaardil
+                            </h3>
+                            <div className="border" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="embed-responsive embed-responsive-21by9">
+                        <iframe
+                          allowFullScreen
+                          frameBorder="0"
+                          src="https://twimap.com/?embed=true&location=59.438698%2C24.729117&zoom=15&distance=500&count=100&min_timestamp=0&max_timestamp=0"
+                        />
+                      </div>
+                    </section>
+                  </div>
+                )}
+
+                <div className="section" />
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="offset-lg-4 col-lg-4 offset-md-3 col-md-6 col-sm-6 col-12 main-section text-center">
-          <div className="row">
-            <div className="col-lg-12 col-sm-12 col-12 profile-header" />
-          </div>
-          <div className="row user-detail">
-            <div className="col-lg-12 col-sm-12 col-12">
-              <img src={man} className="rounded-circle img-thumbnail" />
-              <h5>{this.getPhone()}</h5>
-              <p>
-                <i className="fa fa-map-marker" aria-hidden="true" />{' '}
-                {this.getAddress() || 'Ei ole saadaval'}
-              </p>
-              <hr />
-              <p>
-                <i className="fa fa-compass" aria-hidden="true" />{' '}
-                {this.getLatitude() || 'Ei ole saadaval'}
-              </p>
-              <p>
-                <i className="fa fa-globe" aria-hidden="true" />{' '}
-                {this.getLongitude() || 'Ei ole saadaval'}
-              </p>
-            </div>
-          </div>
-          <div className="row user-social-detail" />
-        </div>
-
-        {this.getLatitude() && this.getLongitude() && (
-          <div>
-            <section>
-              <div className="row section">
-                <div className="col-lg-12 text-center">
-                  <div className="title-box">
-                    <p className="title-alt">Waze</p>
-                    <h3 className="fadeIn animated wow" data-wow-delay=".1s">
-                      Sündmused kaardil
-                    </h3>
-                    <div className="border" />
-                  </div>
-                </div>
-              </div>
-              <div className="embed-responsive embed-responsive-21by9">
-                <iframe
-                  allowFullScreen
-                  frameBorder="0"
-                  src="https://embed.waze.com/iframe?zoom=14&lat=59.438698&lon=24.729117&ct=livemap"
-                />
-              </div>
-            </section>
-
-            <section>
-              <div className="row section">
-                <div className="col-lg-12 text-center">
-                  <div className="title-box">
-                    <p className="title-alt">Twitter</p>
-                    <h3 className="fadeIn animated wow" data-wow-delay=".1s">
-                      Säutsud kaardil
-                    </h3>
-                    <div className="border" />
-                  </div>
-                </div>
-              </div>
-              <div className="embed-responsive embed-responsive-21by9">
-                <iframe
-                  allowFullScreen
-                  frameBorder="0"
-                  src="https://twimap.com/?embed=true&location=59.438698%2C24.729117&zoom=15&distance=500&count=100&min_timestamp=0&max_timestamp=0"
-                />
-              </div>
-            </section>
-          </div>
-        )}
-
-        <div className="section" />
-
         <footer className="section bg-gray footer">
           <div className="container">
             <div className="row">
